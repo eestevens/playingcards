e75a830fbb760430378cd1e3e0bb766e5cffb0dd
eestevens <eedwardstevens@gmail.com>
2018-09-24 00:02:45 -0500

    Adding Deck test methods

--- src/test/java/DeckTest.java
+++ src/test/java/DeckTest.java
@@ -12,8 +12,49 @@ public class DeckTest {
     }
 
     @Test
-    public void testDeck() {
+    public void testDeckSize() {
         int expectedDeckSize = 52;
         Assert.assertEquals(expectedDeckSize, testDeck.returnDeck().length);
+    }
+
+    @Test
+    public void testValues() {
+        int expectedValueCount = 4;
+        Card[] cards = testDeck.returnDeck();
+        for(Value val : Value.values()) {
+            int count = 0;
+            for(int i = 0; i < cards.length ; i ++) {
+                if(cards[i].getValue().equals(val)) {
+                    count++;
+                }
+            }
+            Assert.assertTrue("4 of each card value are expected", count == expectedValueCount);
+
+        }
+    }
+
+    @Test
+    public void testSuits() {
+        int expectedSuitCount = 13;
+        Card[] cards = testDeck.returnDeck();
+        for(Suit suit : Suit.values()) {
+            int count = 0;
+            for(int i = 0; i < cards.length ; i ++) {
+                if(cards[i].getSuit().equals(suit)) {
+                    count++;
+                }
+            }
+            Assert.assertTrue("13 of each card suit are expected", count == expectedSuitCount);
+        }
+    }
+
+    @Test
+    public void testUniqueCards() {
+        Card[] cards = testDeck.returnDeck();
+        for(int i = 0 ; i < cards.length-1; i ++) {
+            for(int j = i + 1; j < cards.length; j++) {
+                Assert.assertTrue("Each card is unique", !cards[i].equals(cards[j]));
+            }
+        }
     }
 }
