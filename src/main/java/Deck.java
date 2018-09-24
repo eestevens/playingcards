a2e1bcb79972cabb4c600fbcf80d345fb3a0d68e
eestevens <eedwardstevens@gmail.com>
2018-09-23 22:41:54 -0500

    Deck Updates

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -1,8 +1,20 @@
 public class Deck {
 
     private Card[] cardDeck;
-    public Deck() {
 
+    public Deck() {
+        int i = 0;
+        int deckSize = Suit.values().length * Value.values().length;
+        cardDeck = new Card[deckSize];
+        for (Suit suit : Suit.values()) {
+            for(Value value : Value.values()) {
+                cardDeck[i++] = new Card(suit, value);
+            }
+        }
+    }
+
+    public Card[] returnDeck() {
+        return cardDeck;
     }
 
     public void shuffle() {
--- src/test/java/DeckTest.java
+++ src/test/java/DeckTest.java
@@ -1,5 +1,19 @@
-/**
- * Created by erikstevens on 9/23/18.
- */
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class DeckTest {
+    private Deck testDeck;
+
+
+    @Before
+    public void initializeDeck() {
+        testDeck = new Deck();
+    }
+
+    @Test
+    public void testDeck() {
+        int expectedDeckSize = 52;
+        Assert.assertEquals(expectedDeckSize, testDeck.returnDeck().length);
+    }
 }
